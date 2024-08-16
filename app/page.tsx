c97c7916eace43971b8937a43fa75c2d96641d33
Momna Ijaz <momna533gb@gmail.com>
2024-08-16 19:31:43 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
+import logo from "@/assets/organic-store-logo5.svg";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="flex">
+        <div>
+          <div>
+            <Link href="/">
+              <Image src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div className="">
+            <Link href="/shop">Everything</Link>
+            <Link href="/product-category/groceries">Groceries</Link>
+            <Link href="/product-category/juice">Juice</Link>
+          </div>
+        </div>
+        <div>
+          <div className="">
+            <Link href="/about">About</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+          <div>
+            <Link href="/cart">
+              <div>0.00</div>
+              <div>Cart</div>
+            </Link>
+          </div>
+          <div>
+            <Link href="/login">Login</Link>
+            <Link href="/signup">Sign Up</Link>
+            <Link href="/logout">Logout</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
